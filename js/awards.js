c79e7332af5b19b016fbf7e013b751eafc0a9d62
TitovAndrey1996 <titovandrey1996@gmail.com>
2024-11-04 15:58:21 +0000

    test js

--- js/awards.js
+++ js/awards.js
@@ -1,8 +1,21 @@
-document.querySelectorAll('.awards__item').forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.classList.add('hovered');
-    });
-    item.addEventListener('mouseout', () => {
-        item.classList.remove('hovered');
-    });
+console.log("awards.js is loaded and running");
+
+document.addEventListener("htmx:afterSwap", (event) => {
+    console.log("htmx:afterSwap event triggered"); // Перевіряємо, чи подія взагалі спрацьовує
+    if (event.detail.target.querySelector('.awards__item')) {
+        console.log("Awards partial loaded. Attaching event listeners.");
+
+        document.querySelectorAll('.awards__item').forEach(item => {
+            item.addEventListener('mouseover', () => {
+                console.log('Mouse over award item');
+                item.classList.add('hovered');
+            });
+            item.addEventListener('mouseout', () => {
+                console.log('Mouse out of award item');
+                item.classList.remove('hovered');
+            });
+        });
+    }
 });
+
+
