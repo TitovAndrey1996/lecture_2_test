4f5ef385a53dce6ed272865706649e7aa2d5c4e3
TitovAndrey1996 <titovandrey1996@gmail.com>
2024-11-04 21:24:59 +0000

    fix js awards

--- js/awards.js
+++ js/awards.js
@@ -1,21 +1,16 @@
 console.log("awards.js is loaded and running");
 
-document.addEventListener("htmx:afterSwap", (event) => {
-    console.log("htmx:afterSwap event triggered"); // Перевіряємо, чи подія взагалі спрацьовує
-    if (event.detail.target.querySelector('.awards__item')) {
-        console.log("Awards partial loaded. Attaching event listeners.");
-
-        document.querySelectorAll('.awards__item').forEach(item => {
-            item.addEventListener('mouseover', () => {
-                console.log('Mouse over award item');
-                item.classList.add('hovered');
-            });
-            item.addEventListener('mouseout', () => {
-                console.log('Mouse out of award item');
-                item.classList.remove('hovered');
-            });
+setTimeout(() => {
+    document.querySelectorAll('.awards__item').forEach(item => {
+        item.addEventListener('mouseover', () => {
+            console.log('Mouse over award item');
+            item.classList.add('hovered');
         });
-    }
-});
+        item.addEventListener('mouseout', () => {
+            console.log('Mouse out of award item');
+            item.classList.remove('hovered');
+        });
+    });
+}, 1000); // Час очікування, можна змінювати залежно від швидкості завантаження
 
 
